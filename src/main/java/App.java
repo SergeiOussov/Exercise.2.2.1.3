27def07564299d10b374003aeb7fbb22effb7a41
Serge <sergei.oussov@yandex.ru>
2023-04-03 16:12:13 +0300

    Решение задачи 2.2.1.3

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,12 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        Cat cat1 = (Cat) applicationContext.getBean("catBean");
+        Cat cat2 = (Cat) applicationContext.getBean("catBean");
+        System.out.println("bean = bean1 is " + (bean == bean2));
+        System.out.println("cat1 = cat2 is " + (cat1 == cat2));
+
     }
 }
